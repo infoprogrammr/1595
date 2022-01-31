57ed26b8e3bc15ba3e39250ece8b21c1290b718a
candidate <candidate@programmr.com>
2022-01-31 08:30:38 +0000

    submit

--- Challenge.cpp
+++ Challenge.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
-/* Start Your Coding Here */
+    int a[6];
+    for(int i=0;i<6;i++){
+        cin>>a[i];
+    }
+    sort(a,a+6);
+    for(int i=0;i<6;i++){
+        int count=0;
+        while(a[i]==a[i+1]){i++; count++;}
+        count++;
+        if(count%2!=0) cout<<a[i]<<" ";
+    }
   return 0;
 }
